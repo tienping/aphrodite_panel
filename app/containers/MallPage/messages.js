914f5e3d4144b7fdb3c331f37bba9a811971e26a
TienPing <tienping@Lims-MacBook-Pro.local>
2017-12-21 12:19:36 +0800

    add mallpage

---
+++ app/containers/MallPage/messages.js
@@ -0,0 +1,13 @@
+/*
+ * Mall Messages
+ *
+ * This contains all the text for the Mall component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+    header: {
+        id: 'app.containers.MallPage.header',
+        defaultMessage: 'This is MallPage container !',
+    },
+});
---
+++ app/containers/MallPage/reducer.js
@@ -0,0 +1,37 @@
+/*
+ *
+ * Mall reducer
+ *
+ */
+
+import { fromJS } from 'immutable';
+import {
+    FETCH_MALL,
+    FETCH_MALL_SUCCESS,
+    FETCH_MALL_FAILED,
+} from './constants';
+
+const initialState = fromJS({});
+
+function mallPageReducer(state = initialState, action) {
+    switch (action.type) {
+        case FETCH_MALL:
+            return state
+                .set('loading', true)
+                .set('error', false);
+        case FETCH_MALL_SUCCESS:
+            return state
+                .set('loading', false)
+                .set('error', false)
+                .setIn(['mall'], action.payload);
+        case FETCH_MALL_FAILED:
+            return state
+                .set('loading', false)
+                .set('error', true)
+                .setIn(['mall'], action.payload);
+        default:
+            return state;
+    }
+}
+
+export default mallPageReducer;
