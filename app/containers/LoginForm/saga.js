3b2bd49e3dcf4d0541bfc327f3d99eac8885d165
TienPing <petwesley@gmail.com>
2018-12-20 18:10:19 +0800

    check is admin before save token

--- app/containers/LoginForm/saga.js
+++ app/containers/LoginForm/saga.js
@@ -15,14 +15,16 @@ export function* doLogin(action) {
         const hash = base64.encode(`${username}:${password}`);
         const response = yield call(apiRequest, 'auth/token', 'post', {}, null, { headers: { 'Authorization': `Basic ${hash}` } });
         if (response && response.ok) {
-            globalScope.token = response.data.token;
-
             const isAdminResponse = yield call(apiRequest, '/view/preview/145', 'post');
             globalScope.isAdmin = !!(isAdminResponse && isAdminResponse.data && isAdminResponse.data.id);
-
-            setCookie(process.env.TOKEN_KEY, globalScope.token);
-            setCookie(process.env.ADMIN_KEY, globalScope.isAdmin);
-            yield put(loginSuccess(response.data.token));
+            if (globalScope.isAdmin) {
+                globalScope.token = response.data.token;
+                setCookie(process.env.TOKEN_KEY, globalScope.token);
+                setCookie(process.env.ADMIN_KEY, globalScope.isAdmin);
+                yield put(loginSuccess(response.data.token));
+            } else {
+                yield put(loginFailed(response.data));
+            }
         } else {
             yield put(loginFailed(response.data));
         }
