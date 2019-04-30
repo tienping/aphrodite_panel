73b6ec66937e99f4ea0bfab91c42fbfe7a3741a1
TienPing <petwesley@gmail.com>
2019-04-30 18:32:03 +0800

    add in TopNavigation

---
+++ app/components/TopNavigation/index.js
@@ -0,0 +1,32 @@
+/**
+*
+* TopNavigation
+*
+*/
+
+import React from 'react';
+import topNavSetting from 'utils/globalTopNavSetting';
+
+import './style.scss';
+
+class TopNavigation extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+    render() {
+        return (
+            <div
+                className="TopNavigation-bar"
+            >
+                <div>
+                    {
+                        Object.keys(topNavSetting).map((key) => <div>{key}</div>)
+                    }
+                </div>
+            </div>
+        );
+    }
+}
+
+TopNavigation.propTypes = {
+
+};
+
+export default TopNavigation;
